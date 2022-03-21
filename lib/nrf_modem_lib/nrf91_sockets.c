50a1bca70f881cab5226c3c5b6e7404513aef423
Håkon Alseth <haal@nordicsemi.no>
2022-03-21 15:57:51 +0100

    lib: nrf_modem: POSIX compatibility on recvfrom()
    If the input buffer "from" and the length fromlen recvfrom() is
    less than the sizeof(struct sockaddr_in) or sizeof(struct sockaddr_in6)
    we should, per POSIX compatibility, truncate the returned per the input buffer size.
    
    Signed-off-by: Håkon Alseth <haal@nordicsemi.no>

--- lib/nrf_modem_lib/nrf91_sockets.c
+++ lib/nrf_modem_lib/nrf91_sockets.c
@@ -810,12 +810,15 @@ static ssize_t nrf91_socket_offload_recvfrom(void *obj, void *buf, size_t len,
 		}
 
 		if (cliaddr->sa_family == NRF_AF_INET) {
-			nrf_to_z_ipv4(from, (struct nrf_sockaddr_in *)cliaddr);
-			*fromlen = sizeof(struct sockaddr_in);
+			struct sockaddr_in tmp_from;
+			*fromlen = MIN(*fromlen, sizeof(struct sockaddr_in));
+			nrf_to_z_ipv4((struct sockaddr *)&tmp_from, (struct nrf_sockaddr_in *)cliaddr);
+			memcpy(from, &tmp_from, *fromlen);
 		} else if (cliaddr->sa_family == NRF_AF_INET6) {
-			nrf_to_z_ipv6(from, (struct nrf_sockaddr_in6 *)
-					  cliaddr);
-			*fromlen = sizeof(struct sockaddr_in6);
+			struct sockaddr_in6 tmp_from;
+			*fromlen = MIN(*fromlen, sizeof(struct sockaddr_in6));
+			nrf_to_z_ipv6((struct sockaddr *)&tmp_from, (struct nrf_sockaddr_in6 *)cliaddr);
+			memcpy(from, &tmp_from, *fromlen);
 		}
 	}
 
